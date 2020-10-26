5c7f360b4ee90780a6a0e8302b230160db7772aa
Christopher Dalziel <christopher.dalziel@lexicondigital.com.au>
2020-10-27 09:28:24 +1100

    01 progress

--- src/exercise/01 retry.js
+++ src/exercise/01 retry.js
@@ -3,86 +3,14 @@
 
 import React from 'react'
 
-// ** 1
-
-// First argument in your reducer is called the "state", the second argument is called the "action"
-// function countReducer(count, step) {
-//   return count + step
-// }
-
-// ** 2
-
-// Count reducer now takes the whole state and the updated version of that state object
-// function countReducer(state, action) {
-//   // Then we merge them together to get our new state values
-//   return ({ ...state, ...action })
-// }
-
-// ** 3
-// function countReducer(state, action) {
-//   // We could have just returned action(state) however by using the typeof with the ternary we're able handle both a function and an object
-//   return ({ ...state, ...(typeof action === 'function' ? action(state) : action) })
-// }
-
-// ** 4
-// *** Failed attempt
-// const countReducer = (state, action) => {
-//   switch (action) {
-//     case 'INCREMENT': return {
-//       ...state, ...(typeof action === 'function' ? action(state) : action)
-//     };
-//     default: return state;
-//   }
-// }
-// *** Copied answer
-function countReducer(state, action) {
-  switch (action.type) {
-    case 'INCREMENT': {
-      return { count: state.count + action.step }
-    }
-    default: {
-      break
-    }
-  }
+function countReducer(state, newState) {
+  return newState
 }
 
-function Counter({ initialCount = 0, step = 1 }) {
-  // ** 1
+function Counter({initialCount = 0, step = 1}) {
+  const [count, setCount] = React.useReducer(countReducer, initialCount)
 
-  // 🐨 replace React.useState with React.useReducer.
-  // 💰 React.useReducer(countReducer, initialCount)
-  // const [count, changeCount] = React.useReducer(countReducer, initialCount)
-  // changeCount is our "dispatch" function
-  // const increment = () => changeCount(step)
-
-  // ** 2
-
-  // const [state, setState] = React.useReducer(countReducer, {
-  //   count: initialCount,
-  // })
-  // const { count } = state
-  // const increment = () => setState({ count: count + step })
-
-  // ** 3
-
-  // const [state, setState] = React.useReducer(countReducer, {
-  //   count: initialCount,
-  // })
-  // const { count } = state
-  // const increment = () =>
-  //   setState(currentState => ({ count: currentState.count + step }))
-
-  // ** 4
-  const [state, dispatch] = React.useReducer(countReducer, {
-    count: initialCount,
-  })
-  const { count } = state
-  const increment = () => dispatch({ type: 'INCREMENT', step })
-
-  // 💰 you can write the countReducer function so you don't have to make any
-  // changes to the next two lines of code! Remember:
-  // The 1st argument is called "state" - the current value of count
-  // The 2nd argument is called "newState" - the value passed to setCount
+  const increment = () => setCount(count + step)
   return <button onClick={increment}>{count}</button>
 }
 
