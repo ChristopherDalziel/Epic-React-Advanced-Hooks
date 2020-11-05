fc636847374719ffd55f18a6d7dff848d35cbea2
Christopher Dalziel <christopher.dalziel@lexicondigital.com.au>
2020-11-05 17:25:55 +1100

    03.01 completed

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -10,13 +10,22 @@ function CountProvider(props) {
   const value = [count, setCount]
   return <CountContext.Provider value={value} {...props} />
 }
+
+function useCount() {
+  const context = React.useContext(CountContext)
+  if (!context) {
+    throw new Error('useCount must be used within a CountProvider')
+  }
+  return context
+}
+
 function CountDisplay() {
-  const [count] = React.useContext(CountContext)
+  const [count] = useCount()
   return <div>{`The current count is ${count}`}</div>
 }
 
 function Counter() {
-  const [, setCount] = React.useContext(CountContext)
+  const [, setCount] = useCount()
   const increment = () => setCount(c => c + 1)
   return <button onClick={increment}>Increment count</button>
 }
@@ -31,5 +40,15 @@ function App() {
     </div>
   )
 }
+
+// **Extra Credit 1** - Alls this does is throw a bunch of errors, to be successful we should still use the above.
+// function App() {
+//   return (
+//     <div>
+//       <CountDisplay />
+//       <Counter />
+//     </div>
+//   )
+// }
 
 export default App
